42d53bb691d634046add2de286b4589d09044881
firefox3200 <141912868+firefox3200@users.noreply.github.com>
2024-07-17 18:58:38 +0500

    add tests--amend

--- test_game.py
+++ test_game.py
@@ -5,7 +5,12 @@ class TestGuessTheNumber(unittest.TestCase):
 
     def test_get_player1_number_valid(self):
         self.assertEqual(get_player1_number(50), 50)
+    
+    def test_get_player1_number_invalid_low(self):
+        with self.assertRaises(ValueError):
+            get_player1_number(0)
 
+   
 
 if __name__ == "__main__":
     unittest.main()
